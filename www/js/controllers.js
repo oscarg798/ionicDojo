750350c92a71773a77469e9bb12ad2b57275d7d1
oscar david gallon rosero <oscarg798@gmail.com>
2015-05-06 21:44:39 -0500

    log in

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,21 +1,66 @@
-angular.module('reader.controllers', ['ionic','ngCordova'])
-.controller('readerCtrl', function($scope, $cordovaBarcodeScanner) {
-	$scope.leerCodigo=  function() {
+angular.module('reader.controllers', ['ionic', 'ngCordova'])
+    .controller('readerCtrl', function($scope, $cordovaBarcodeScanner) {
+        $scope.leerCodigo = function() {
 
-		alert('presiono');
-		// body...
-		$cordovaBarcodeScanner.scan().then(function (img) {
-			// body...
-				alert(img.text);
+            alert('presiono');
+            // body...
+            $cordovaBarcodeScanner.scan().then(function(img) {
+                // body...
+                alert(img.text);
 
-		}, function (err) {
-			// body...
-			alert('Error : ' +err);
-		});
-	}
-}).controller('panelCtrl', function ($scope) {
-	$scope.sayHello= function  (name) {
-		// body...
-		alert('Hola ' + name);
-	}
-});
+            }, function(err) {
+                // body...
+                alert('Error : ' + err);
+            });
+        }
+    }).controller('panelCtrl', function($scope) {
+        var people = [{
+            username: 'oscarg798',
+            password: '123456'
+
+        }, {
+            username: 'stalbeal',
+            password: 'saba10'
+
+        }];
+        $scope.log = false;
+
+        $scope.users = people;
+
+        $scope.logIn = function(username, password) {
+            // body...
+            //alert('users ' + JSON.stringify($scope.users));
+            if (!username){
+                alert('Debe ingresar nombre de usuario');
+                 return;
+            }
+
+            if(!password){
+            	 alert('Debe ingresar una contraseña');
+                 return;
+            }
+
+            $scope.users.forEach(function(user) {
+                if (user.username == username && user.password == password) {
+                    $scope.log = true;
+                    return;
+                }
+            });
+
+            if (!$scope.log)
+                alert("Los datos ingresados no son correctos verifique por favor");
+
+        }
+
+        $scope.isLogged= function  (argument) {
+        	// body...
+        	return $scope.log;
+        }
+
+        $scope.sayHello = function(name) {
+            // body...
+            alert('Hola ' + name);
+        }
+
+
+    });
